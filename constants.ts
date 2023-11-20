a4e04692e11e8ffd736aa94780ef5d58f99fa117
adityasuman2025 <adityasuman2025@gmail.com>
2023-11-20 12:54:27 +0530

    env variable name updated

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
-export const FB_API_BASE_URL = process.env.REACT_APP_DB_URL;
-export const FB_API_BASE_URL_LC = process.env.REACT_APP_DB_URL_LC;
+export const FB_API_BASE_URL = process.env.REACT_APP_FB_API_BASE_URL;
+export const FB_API_BASE_URL_LC = process.env.REACT_APP_FB_API_BASE_URL_LC;
 
 export const ENCRYPTION_KEY = process.env.REACT_APP_ENCRYPTION_KEY;
 export const ENCRYPTION_KEY_LC = process.env.REACT_APP_ENCRYPTION_KEY_LC;
